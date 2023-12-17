efd825085b9a26148a3cb36bad328a37811f5e5b
Divakar-R <148629037+Divakar-R@users.noreply.github.com>
2023-12-17 12:42:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var fname=document.getElementById("fname")
+
+function foo(){
+    var fname=document.getElementById("fname").value;
+    var mname=document.getElementById("mname").value;
+    var lname=document.getElementById("lname").value;
+    var res = document.getElementById("email").value;
+    console.log(fname);
+    console.log(mname);
+    console.log(lname);
+    console.log(res);
+    }
